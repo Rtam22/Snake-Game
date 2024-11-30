70606b3844477d95ce0ce8d68ae9fb5097c44bb5
Regan <regantam92@gmail.com>
2024-11-30 14:05:32 +1100

    Refactored Code

--- gameLogic.js
+++ gameLogic.js
@@ -1,85 +1,61 @@
 import Player from '/player.js'
+import Board from '/Board.js'
 
-const startButton = document.getElementById('start-button')
-const restartButton = document.getElementById('restart-button')
-
-const boardCols = 20
-const boardRows = 20
-const boardSize = boardCols * boardRows
-const player = new Player(1, 'right', boardSize/2)
-const boardEdges = [
-    {border: 'top', borderLocation: []},
-    {border: 'bottom', borderLocation: []},
-    {border: 'left', borderLocation: []},
-    {border: 'right', borderLocation: []}
-]
-
-
-function mapBoardEdges () {
-    let left = 1 - boardCols
-    let right = 0
-    for(let i = 0; i < boardCols; i++){
-        boardEdges[0].borderLocation.push(i + 1)
-        boardEdges[1].borderLocation.unshift(boardSize - (i))
+class GameLogic {
+    constructor(){
+        this.startButton = document.getElementById('start-button')
+        this.restartButton = document.getElementById('restart-button')
+        this.board = new Board(20,20)
+        this.player = new Player(1, 'right', this.board.boardSize/2)
+        this.assignControls()
     }
-    for(let i = 0; i < boardRows; i++){
-        boardEdges[2].borderLocation.push(left += boardCols)
-        boardEdges[3].borderLocation.push(right += boardCols)
-    }
-}
 
-function generateBoard () {
-    for(let i = 0; i < boardSize; i++){
-        const divBox = document.createElement("div")
-        divBox.classList.add('grid-box')
-        divBox.setAttribute('id', i + 1)
-        document.querySelector('.game-grid').appendChild(divBox)
-    }
-}
-
-function assignControls (){
-    addEventListener('keydown', (e => {
-        switch(e.key){
-            case 'ArrowRight':
-                if(player.direction != 'Left')
-                {
-                    player.setDirection('Right')
-                }
-                break;
-            case 'ArrowLeft':
-                if(player.direction != 'Right')
+    assignControls (){
+        addEventListener('keydown', (e => {
+            switch(e.key){
+                case 'ArrowRight':
+                    if(this.player.direction != 'Left')
                     {
-                        player.setDirection('Left')
+                        this.player.setDirection('Right')
                     }
-                break;
-            case 'ArrowUp':
-                if(player.direction != 'Down')
-                    {
-                        player.setDirection('Up')
-                    }    
-                player.setDirection('Up')
-                break;
-            case 'ArrowDown':
-                if(player.direction != 'Up')
-                    {
-                        player.setDirection('Down')
-                    }    
-                break;
-                
+                    break;
+                case 'ArrowLeft':
+                    if(this.player.direction != 'Right')
+                        {
+                            this.player.setDirection('Left')
+                        }
+                    break;
+                case 'ArrowUp':
+                    if(this.player.direction != 'Down')
+                        {
+                            this.player.setDirection('Up')
+                        }    
+                        this.player.setDirection('Up')
+                    break;
+                case 'ArrowDown':
+                    if(this.player.direction != 'Up')
+                        {
+                            this.player.setDirection('Down')
+                        }    
+                    break;
+            }
+        }))
+    }
+
+        startGame (){
+
         }
-    }))
-}
 
-function startGame () {
+        restartGame (){
+            
+        }
 
-}
+        initializeGame (){      
+            this.board.generateBoard()
+            this.startButton.addEventListener('click', this.startGame())
+            this.restartButton.addEventListener('click', this.restartGame())
+        }
+    }
 
-function restartGame () {
-    
-}
-
-generateBoard()
-mapBoardEdges()
-assignControls()
-startButton.addEventListener('click', startGame())
-restartButton.addEventListener('click', restartGame())
+    const game = new GameLogic()
+    game.initializeGame()
